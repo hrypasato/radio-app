a2be495d64b39ec999c0c8f35ec628beac6b5eae
Henry Quinde <hrypasato@gmail.com>
2022-05-04 21:44:38 -0500

    feat(card): add country to radio station card

--- src/components/cards/card.jsx
+++ src/components/cards/card.jsx
@@ -18,7 +18,7 @@ export const Card = ({ children, ...props }) => {
       <img className="object-cover w-full h-16 rounded-t-lg md:h-32 md:w-32 md:rounded-none md:rounded-l-lg" src={item.favicon ? item.favicon : IMAGE_URL } alt="" />
       <div className="flex flex-col justify-between p-4 leading-normal">
         <h5 className="mb-2 text-lg font-bold tracking-tight text-gray-900">{item.name}</h5>
-        <p className="mb-3 font-normal text-gray-700">Radio tags</p>
+        <p className="mb-3 font-normal text-gray-700">{item.country}</p>
       </div>
     </div>
 
