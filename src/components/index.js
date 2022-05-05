1c30c27859afb1ac011085433a1ec9bcdc5648ff
Henry Quinde <hrypasato@gmail.com>
2022-05-04 21:34:35 -0500

    refactor(components): remove button component

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,7 @@
-import { Button } from "./buttons/button";
 import { Card } from "./cards/card";
 import { RadioPLayer } from "./media/player";
 
 export {
-    Button,
     Card,
     RadioPLayer
 }
